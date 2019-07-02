3d38ff9233d229db6f9caa5e8eab731d4535e003
Iunius118 <18502177+Iunius118@users.noreply.github.com>
2019-07-02 21:12:32 +0900

    Fix mod class

--- src/main/java/com/github/iunius118/type18grenadelauncher/Type18GrenadeLauncher.java
+++ src/main/java/com/github/iunius118/type18grenadelauncher/Type18GrenadeLauncher.java
@@ -1,13 +1,13 @@
 package com.github.iunius118.type18grenadelauncher;
 
+import com.github.iunius118.type18grenadelauncher.client.ClientEventHandler;
 import com.github.iunius118.type18grenadelauncher.entity.Type18GrenadeEntity;
+import com.github.iunius118.type18grenadelauncher.item.Type18GrenadeItem;
+import com.github.iunius118.type18grenadelauncher.item.Type18GrenadeLauncherItem;
 import net.minecraft.entity.Entity;
 import net.minecraft.item.Item;
-import net.minecraft.util.ResourceLocation;
-import net.minecraft.util.math.Vec3d;
-import net.minecraft.world.World;
-import net.minecraft.world.chunk.IChunkProvider;
-import net.minecraft.world.gen.ChunkProviderServer;
+import net.minecraft.util.math.RayTraceResult;
+import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.event.entity.EntityEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -15,7 +15,7 @@ import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.registry.EntityEntry;
 import net.minecraftforge.fml.common.registry.EntityEntryBuilder;
-import net.minecraftforge.fml.common.registry.EntityRegistry;
+import net.minecraftforge.fml.common.registry.GameRegistry;
 import org.apache.logging.log4j.Logger;
 
 @Mod(
@@ -30,18 +30,33 @@ public class Type18GrenadeLauncher {
     public static final String MOD_VERSION = "1.12-2-1.0.0.0";
     public static final String MOD_DEPENDENCIES = "required-after:forge@[1.12.2-14.23.3.2768,)";
 
-    public static final boolean DEBUG = true;
+    public static final boolean DEBUG = false;
 
     public static Logger logger;
     public static final Type18GrenadeLauncherConfig config = new Type18GrenadeLauncherConfig();
 
+    @GameRegistry.ObjectHolder(MOD_ID)
+    public static class Items {
+        public static final Item TYPE_18_GRENADE_LAUNCHER = null;
+        public static final Item TYPE_18_GRENADE = null;
+    }
+
+
     @Mod.EventHandler
     public void preInit(FMLPreInitializationEvent event) {
         logger = event.getModLog();
+
+        if (event.getSide().isClient()) {
+            MinecraftForge.EVENT_BUS.register(new ClientEventHandler());
+        }
     }
 
     @SubscribeEvent
     public static void registerItems(RegistryEvent.Register<Item> event) {
+        event.getRegistry().registerAll(
+                new Type18GrenadeLauncherItem().setRegistryName("type_18_grenade_launcher").setTranslationKey(Type18GrenadeLauncher.MOD_ID + ".type_18_grenade_launcher"),
+                new Type18GrenadeItem().setRegistryName("type_18_grenade").setTranslationKey(Type18GrenadeLauncher.MOD_ID + ".type_18_grenade")
+        );
 
     }
 
@@ -53,21 +68,17 @@ public class Type18GrenadeLauncher {
     }
 
     @SubscribeEvent
-    public static void onEnteringChunk(EntityEvent.EnteringChunk event) {
+    public static void canEntityUpdate(EntityEvent.CanUpdate event) {
         Entity entity = event.getEntity();
-        World world = entity.world;
 
-        if (!world.isRemote && entity instanceof Type18GrenadeEntity) {
-            if (Type18GrenadeLauncher.config.common.killGrenadeWhichEnteringUnloadedChunk) {
-                ChunkProviderServer chunkProvider = (ChunkProviderServer) world.getChunkProvider();
-
-                if (!chunkProvider.chunkExists(event.getNewChunkX(), event.getOldChunkZ())) {
-                    // Grenade is entering unloaded chunk
-                    Type18GrenadeEntity grenadeEntity = (Type18GrenadeEntity) entity;
-                    grenadeEntity.setDead();
-                    grenadeEntity.logOnDead("onEnteringUnloadedChunk", new Vec3d(grenadeEntity.posX, grenadeEntity.posY, grenadeEntity.posZ));
-                }
-            }
+        if (entity instanceof Type18GrenadeEntity
+                && !entity.world.isRemote
+                && Type18GrenadeLauncher.config.common.destroyGrenadeWhichEnteringUnloadedChunk
+                && !event.getCanUpdate()) {
+            // Grenade is entering unloaded chunk
+            Type18GrenadeEntity grenadeEntity = (Type18GrenadeEntity) entity;
+            // Detonate grenade
+            grenadeEntity.onImpact(new RayTraceResult(grenadeEntity));
         }
     }
 }
